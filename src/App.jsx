1bc0826862a33575b07106afaa9eb11c516aaf4d
Roy543 <sahilsas88@gmail.com>
2024-03-18 09:38:35 -0700

    subscribe and footer section implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,12 @@ const App = () => (
     <section className="bg-pale-blue padding">
       <CustomerReviews />
     </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className=" bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 

0edf1e9115b9dd7a9c2ded7c2807583bfbed18af
Roy543 <sahilsas88@gmail.com>
2024-03-18 05:57:02 -0700

    Navbar implementation completed

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import Service from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQaulity from "./SuperQaulity";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    PopularProduct,
+    Footer,
+    Service,
+    SpecialOffer,
+    Subscribe,
+    SuperQaulity,
+    CustomerReviews
+} 
